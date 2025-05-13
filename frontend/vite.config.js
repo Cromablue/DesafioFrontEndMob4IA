629bc54a5257d4c382c9948814864f92aa0d937c
CROMABLUE <jsmoura.dev@gmail.com>
2025-05-13 14:51:36 -0400

    :wrench: chore: vite config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   server: {
-    host: '0.0.0.0', // pro docker acessar aq
+    host: '0.0.0.0', // para acesso via Docker
     port: 5173,
+  },
+  build: {
+    rollupOptions: {
+      // Normalmente não é necessário externalizar 'recharts',
+      // mas se quiser testar, descomente a linha abaixo:
+      // external: ['recharts'],
+    },
   },
 })
